a3a1e9d3e525bfb4ef1d43a17b389bd9ecfb5b89
mks257 <138339735+mks257@users.noreply.github.com>
2025-05-15 12:09:10 -0500

    created manager.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Band } = require('./models/Band')
 const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
+const { Manager } = require('./models/Manager')
 // Define associations here
 
 
